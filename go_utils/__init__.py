357204e17a373bdc5b16be20b0258f4a341d251f
Matteo <mateus.sakata@gmail.com>
2021-05-10 10:56:23 -0500

    Add package documentation

--- go_utils/__init__.py
+++ go_utils/__init__.py
@@ -1,1 +1,11 @@
 from go_utils.download import *
+
+__doc__ = """
+go_utils contains utilities for interfacing with the GLOBE Observer datasets, primarily the Mosquito Habitat Mapper and Landcover.
+
+Main Features
+-------------
+Here are the major utilities provided by the package:
+
+- Easy downloading of the GLOBE Observer datasets as Pandas DataFrames
+"""
