ff58c61ee4a50e654b413b6a57b33d56f55ec749
Matteo <mateus.sakata@gmail.com>
2021-05-12 17:33:22 -0500

    Improve download methods and tests

--- go_utils/download.py
+++ go_utils/download.py
@@ -6,7 +6,7 @@ import logging
 from go_utils.info import *
 
 
-def parse_api_data(response_json, country_names):
+def parse_api_data(response_json):
     try:
         results = response_json["results"]
         df = pd.DataFrame(results)
@@ -19,16 +19,6 @@ def parse_api_data(response_json, country_names):
     # Drop the previously nested data column
     df = df.drop("data", 1)
 
-    # Country Filters
-    if country_names and len(country_names) > 0:
-        country_filt = np.vectorize(lambda x: x in country_names)
-        df = df[country_filt(df["countryName"].to_numpy())]
-        df.reset_index(drop=True, inplace=True)
-        if len(df) == 0:
-            print(
-                "WARNING: There are no observations from your country(s) with your given parameters."
-            )
-
     # Display the dataframe
     return df
 
@@ -37,7 +27,6 @@ def get_api_data(
     protocol,
     start_date=start_date,
     end_date=end_date,
-    country_names=[],
     latlon_box={"min_lat": -90, "max_lat": 90, "min_lon": -180, "max_lon": 180},
 ):
     """Utility function for interfacing with the GLOBE API.
@@ -51,8 +40,6 @@ def get_api_data(
                  The desired start date of the dataset in the format of (YYYY-MM-DD).
     end_date : str, default= today's date in YYYY-MM-DD form.
                The desired end date of the dataset in the format of (YYYY-MM-DD).
-    country_names : list of str, default= []
-                    The desired countries of the dataset. If left empty, the dataset is not filtered by countries. Note that this may not be accurate due to entries in the GLOBE Observer Dataset that lack country names.
     latlon_box : dict of {str, double}, optional
                  The longitudes and latitudes of a bounding box for the dataset. The minimum/maximum latitudes and longitudes must be specified with the following keys: "min_lat", "min_lon", "max_lat", "max_lon". The default value specifies all latitude and longitude coordinates.
 
@@ -87,4 +74,4 @@ def get_api_data(
             "Failed to get data from the API. Double check your specified settings to make sure they are valid."
         )
 
-    return parse_api_data(response.json(), country_names)
+    return parse_api_data(response.json())
--- go_utils/tests/download_test.py
+++ go_utils/tests/download_test.py
@@ -31,4 +31,128 @@ def test_globe_down():
     }"""
 
     with pytest.raises(RuntimeError, match="down"):
-        parse_api_data(json.loads(globe_down_json), [])
+        parse_api_data(json.loads(globe_down_json))
+
+
+def test_globe_normal():
+
+    sample_lc_json = """
+    {
+    "count": 1710,
+    "message": "success",
+    "results": [
+        {
+        "protocol": "land_covers",
+        "measuredDate": "2010-10-05",
+        "createDate": "2021-03-29T10:43:01",
+        "updateDate": "2021-03-29T10:43:01",
+        "publishDate": "2021-03-29T10:49:19",
+        "organizationId": 326812,
+        "organizationName": "Escuela de Educacion  Media N 8",
+        "siteId": 132,
+        "siteName": "VICENTE LOPEZ:LCS-01",
+        "countryName": "Argentina",
+        "countryCode": "ARG",
+        "latitude": -34.552,
+        "longitude": -58.483,
+        "elevation": 42,
+        "pid": 187086150,
+        "data": {
+            "landcoversDownwardPhotoUrl": null,
+            "landcoversEastExtraData": null,
+            "landcoversEastPhotoUrl": null,
+            "landcoversMucCode": "M91",
+            "landcoversUpwardPhotoUrl": null,
+            "landcoversEastCaption": null,
+            "landcoversWestClassifications": null,
+            "landcoversNorthCaption": null,
+            "landcoversNorthExtraData": null,
+            "landcoversDataSource": "GLOBE Data Entry Site Definition",
+            "landcoversDryGround": null,
+            "landcoversSouthClassifications": null,
+            "landcoversWestCaption": null,
+            "landcoversNorthPhotoUrl": null,
+            "landcoversUpwardCaption": null,
+            "landcoversDownwardExtraData": null,
+            "landcoversEastClassifications": null,
+            "landcoversMucDetails": null,
+            "landcoversMeasuredAt": "2010-10-05T00:00:00",
+            "landcoversDownwardCaption": null,
+            "landcoversSouthPhotoUrl": null,
+            "landcoversMuddy": null,
+            "landcoversWestPhotoUrl": null,
+            "landcoversStandingWater": null,
+            "landcoversLeavesOnTrees": null,
+            "landcoversUserid": null,
+            "landcoversSouthExtraData": null,
+            "landcoversSouthCaption": null,
+            "landcoversRainingSnowing": null,
+            "landcoversUpwardExtraData": null,
+            "landcoversWestExtraData": null,
+            "landcoversLandCoverId": 12736,
+            "landcoversMucDescription": "Urban, Residential",
+            "landcoversSnowIce": null,
+            "landcoversNorthClassifications": null,
+            "landcoversFieldNotes": "Urban, Residential :: please replace with Land Cover Site Comments"
+        }
+        },
+        {
+        "protocol": "land_covers",
+        "measuredDate": "2012-05-11",
+        "createDate": "2021-03-29T10:43:12",
+        "updateDate": "2021-03-29T10:43:12",
+        "publishDate": "2021-03-29T10:49:19",
+        "organizationId": 326053,
+        "organizationName": "Domingo Faustino Sarmiento",
+        "siteId": 133,
+        "siteName": "Mision Baja:LCS-01",
+        "countryName": "Argentina",
+        "countryCode": "ARG",
+        "latitude": -54.4892,
+        "longitude": -68.18545,
+        "elevation": -12.2,
+        "pid": 187086632,
+        "data": {
+            "landcoversDownwardPhotoUrl": null,
+            "landcoversEastExtraData": "null",
+            "landcoversEastPhotoUrl": "https://data.globe.gov/system/photos/2012/11/03/4070/original.JPG",
+            "landcoversMucCode": "M94",
+            "landcoversUpwardPhotoUrl": null,
+            "landcoversEastCaption": "",
+            "landcoversWestClassifications": null,
+            "landcoversNorthCaption": "",
+            "landcoversNorthExtraData": "null",
+            "landcoversDataSource": "GLOBE Data Entry Site Definition",
+            "landcoversDryGround": null,
+            "landcoversSouthClassifications": null,
+            "landcoversWestCaption": "",
+            "landcoversNorthPhotoUrl": "https://data.globe.gov/system/photos/2012/11/03/4073/original.JPG",
+            "landcoversUpwardCaption": null,
+            "landcoversDownwardExtraData": null,
+            "landcoversEastClassifications": null,
+            "landcoversMucDetails": null,
+            "landcoversMeasuredAt": "2012-05-11T00:00:00",
+            "landcoversDownwardCaption": null,
+            "landcoversSouthPhotoUrl": "https://data.globe.gov/system/photos/2012/11/03/4075/original.JPG",
+            "landcoversMuddy": null,
+            "landcoversWestPhotoUrl": "https://data.globe.gov/system/photos/2012/11/03/4076/original.JPG",
+            "landcoversStandingWater": null,
+            "landcoversLeavesOnTrees": null,
+            "landcoversUserid": null,
+            "landcoversSouthExtraData": "null",
+            "landcoversSouthCaption": "",
+            "landcoversRainingSnowing": null,
+            "landcoversUpwardExtraData": null,
+            "landcoversWestExtraData": "null",
+            "landcoversLandCoverId": 12737,
+            "landcoversMucDescription": "Urban, Other",
+            "landcoversSnowIce": null,
+            "landcoversNorthClassifications": null,
+            "landcoversFieldNotes": "Urban, Other :: SCRC data (please do not delete me!)   SCRC data (please do not delete me!)   SCRC data (please do not delete me!) el situo de estudio esta medianamente poblada, la superficie es de pasto muy cerca de la Bahia y con calles de tierra."
+        }
+        }
+    ]
+    }
+    """
+    df = parse_api_data(json.loads(sample_lc_json))
+    assert not df.empty
